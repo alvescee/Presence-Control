13156d8d730cf7585805e4e3e69c94ff166d10be
alvescee <alvescauaferreira@icloud.com>
2024-10-19 17:24:17 -0300

    Adição de funcionalidade para ler um chip
    O chip é lido e salvo no chip da sessão atual.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,66 @@
+/**
+ * @author Cauã Alves
+ * @link https://github.com/alvescee/Presence-Control
+*/
+
+// Imports
+
 #include <SPI.h>
 #include <MFRC522.h>
-#include <string>
+#include <string.h>
+
+// Get pin
+
+#define RST_PIN 22
+#define SS_PIN 5
+
+// Static variables
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+String identifier = "";
+
+// Injection
+
+void setSessionID();
+
+/**
+ * Default functions
+*/
 
 void setup() {
+
+    // By default
     Serial.begin(115200);
+    SPI.begin();
+    rfid.PCD_Init();
+
 }
 
 void loop() {
-    Serial.println("Initial project");
-    delay(1000);
+
+    // The card already or not have card in the rfid
+    if (!(rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial())) return;
+
+    // Read card and save in session
+    setSessionID();
+    Serial.println(identifier);
+
+    // Finish read
+    rfid.PICC_HaltA();
+    
+}
+
+/**
+ * Domain functions
+*/
+
+void setSessionID () {
+
+    identifier = "";
+
+    for (byte i = 0; i < rfid.uid.size; i++) {
+        identifier += rfid.uid.uidByte[i], HEX;
+    }
+    
 }
