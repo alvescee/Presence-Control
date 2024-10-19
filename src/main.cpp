d6ba63204b17d696a1861ca8be6b12b4905b7213
Cauã Alves <alvescauaferreira@icloud.com>
2024-10-19 15:57:06 -0300

    Projeto inicial
    Configuração do platform.io para instalar bibliotecas necessárias para o projeto.

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <string>
+
+void setup() {
+    Serial.begin(115200);
+}
+
+void loop() {
+    Serial.println("Initial project");
+    delay(1000);
+}
